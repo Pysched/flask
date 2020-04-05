23276787623626e852fb4a7daa29557f60522481
Pysched <murphyjdarren@gmail.com>
2020-04-05 17:23:50 +0000

    add files

--- run.py
+++ run.py
@@ -10,7 +10,10 @@ def hello():
 @app.route("/about")
 def about():
     return render_template("about.html")
-    
+@app.route("/contact")
+def about():
+    return render_template("contact.html")
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
